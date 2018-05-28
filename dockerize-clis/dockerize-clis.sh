2bc523e22b5210162ce9a1ce25fe75781b1e827a
Djuuu <julien@tardot.net>
2018-05-28 10:45:30 +0200

    Dockerize-CLIs - utility functions

--- dockerize-clis/dockerize-clis.sh
+++ dockerize-clis/dockerize-clis.sh
@@ -43,3 +43,30 @@ source $CURRENT_DIR/dockerize-clis-node.sh
 
 # NodeJS-related commands
 source $CURRENT_DIR/dockerize-clis-php.sh
+
+
+function docker_clis_pull {
+    docker pull php:$PHP_VERSION-cli
+    docker pull composer
+    docker pull node:$NODE_VERSION
+}
+
+function docker_clis_env {
+    echo "DOCKERIZE_PHP      : $DOCKERIZE_PHP"
+    echo "DOCKERIZE_COMPOSER : $DOCKERIZE_COMPOSER"
+    echo "DOCKERIZE_PHPUNIT  : $DOCKERIZE_PHPUNIT"
+    echo "DOCKERIZE_NODE     : $DOCKERIZE_NODE"
+    echo "DOCKERIZE_NPM      : $DOCKERIZE_NPM"
+    echo "DOCKERIZE_YARN     : $DOCKERIZE_YARN"
+    echo ""
+    echo "PHP_VERSION        : $PHP_VERSION"
+    echo "PHP_PUBLISH_PORTS  : $PHP_PUBLISH_PORTS"
+    echo "PHP_USER           : $PHP_USER"
+    echo ""
+    echo "NODE_VERSION       : $NODE_VERSION"
+    echo "NODE_PUBLISH_PORTS : $NODE_PUBLISH_PORTS"
+    echo "NODE_USER          : $NODE_USER"
+    echo "NODE_USER_HOME     : $NODE_USER_HOME"
+    echo ""
+    echo "DOCKER_ETC_VOLUMES : $DOCKER_ETC_VOLUMES"
+}
