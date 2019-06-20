59c0794048f8911d04796bca00e6cb97e17234a0
Djuuu <julien@tardot.net>
2019-06-20 11:20:57 +0200

    Prompt - Add new line on narrow terminals

--- .bash_prompt/prompt.sh
+++ .bash_prompt/prompt.sh
@@ -2,10 +2,12 @@
 . ~/.bash_prompt/colors.sh
 . ~/.bash_prompt/git.sh
 . ~/.bash_prompt/separator.sh
+. ~/.bash_prompt/newline-narrow-prompt.sh
 
 function prompt_command {
 	git_prompt_command
 	separator_prompt_command
+	newline_narrow_prompt_command
 }
 
 PROMPT_COMMAND=prompt_command
