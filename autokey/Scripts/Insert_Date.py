66be131e1a30e44fece68b2e0422064f01bbc4b9
Djuuu <julien@tardot.net>
2022-02-08 18:56:40 +0100

    Autokey - convert date&time from scripts to phrases

--- autokey/Scripts/Insert_Date.py
+++
@@ -1,4 +0,0 @@
-
-dateValue=system.exec_command("date '+%Y-%m-%d'")
-
-keyboard.send_keys(dateValue)
--- autokey/Scripts/Insert_Time.py
+++
@@ -1,4 +0,0 @@
-
-timeValue=system.exec_command("date '+%H:%M:%S'")
-
-keyboard.send_keys(timeValue)
